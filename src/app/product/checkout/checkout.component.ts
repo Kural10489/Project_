eaf276889fa9584e6f048f1f71c6abf80ed7776c
Asplap3275 <kuralanban.karthi@aspiresys.com>
2023-02-06 17:29:14 +0530

    working on order page

--- src/app/product/checkout/checkout.component.ts
+++ src/app/product/checkout/checkout.component.ts
@@ -8,14 +8,15 @@ import { HttpService } from 'src/app/shared/service/http.service';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
-
+public orderDetails:any=[];
   constructor(private cart:CartService,public http:HttpService){
-
+   this.orderDetails=new Set(this.http.DispatchProductsDetails);
+   console.log(this.http.DispatchProductsDetails);
+    console.log(this.orderDetails);
 
   }
   ngOnInit(): void {
     this.http.getDispatchProductDetails();
-
 
   }
 
